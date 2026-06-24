f2b7ecaec9b2502e428ecf61f2daae10b4668447
Suyash <suyash192@gmail.com>
2026-06-24 23:40:41 +0530

    Remove devices from modelsWithDoViHdr10PlusBug list.

--- app/src/main/java/com/github/damontecres/wholphin/util/profile/KnownDefects.kt
+++ app/src/main/java/com/github/damontecres/wholphin/util/profile/KnownDefects.kt
@@ -8,11 +8,7 @@ import android.os.Build
  * List of device models with known HEVC DoVi/HDR10+ playback issues.
  */
 private val modelsWithDoViHdr10PlusBug =
-    listOf(
-        "AFTKRT", // Amazon Fire TV 4K Max (2nd Gen)
-        "AFTKA", // Amazon Fire TV 4K Max (1st Gen)
-        "AFTKM", // Amazon Fire TV 4K (2nd Gen)
-    )
+    listOf<String>()
 
 /**
  * List of device models with known playback issues when 8-channel EAC3 is copied into MPEG-TS.
